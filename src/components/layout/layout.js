e26a62e6235e21157226b3f24c63b87b9c30205b
Fidalgo <pedrofidalgo7@gmail.com>
2019-05-31 21:08:26 +0100

    moved fixed to the VH on mobile to the layout component, and added event listener to listen for resize on the window and update the css variable

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import styled from 'styled-components';
@@ -25,6 +25,22 @@ const StyledMain = styled.main`
 `;
 
 const Layout = ({ notOnePageSection, children }) => {
+  // FIX FOR VH ON MOBILE
+  const changeVhVariable = () => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    const vh = typeof window !== 'undefined' && window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    typeof document !== 'undefined' &&
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  // Run the function to change the VH variable when the browser is resized
+  useEffect(() => {
+    changeVhVariable();
+    window.addEventListener('resize', changeVhVariable);
+    return () => window.removeEventListener('resize', changeVhVariable);
+  }, [changeVhVariable]);
+
   return (
     <ThemeProvider theme={theme}>
       <Wrapper>
